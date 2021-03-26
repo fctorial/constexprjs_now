0d300d904eeec2ba791fe8425a8a26121b7d6abc
Sagar Tiwari <fctorial@gmail.com>
2021-03-27 04:39:12 +0530

    build script update

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 rm -rf ../constexprjs_now_deployment/*
-../ConstexprJS/constexpr.js "$@" --input . --output ../constexprjs_now_deployment --exclusion /_template.html --exclusion /posts/md_generator.html --exclusion /collections
+constexpr.js "$@" --input . --output ../constexprjs_now_deployment --exclusion /_template.html --exclusion /posts/md_generator.html --exclusion /collections
